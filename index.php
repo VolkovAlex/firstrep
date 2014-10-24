5a35b6bc69dfc30733f6327d204cd18e55567265
Alexander <alex.v.volkov@yandex.ru>
2014-10-24 15:28:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,6 @@
     echo 'Hello, friends!'; // Git test 3
     echo 'Hello, Sanek!'; // Git test 4
     echo 'Hello, programmer )!'; // Git test 5
+    
+    echo 'Hi, Alex! )'; // Git test pull
 ?>
